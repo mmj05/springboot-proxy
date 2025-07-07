76ded497289dafd4cf76b13234f8c994dd55e764
muhaiminjobayer <mmj0993@gmail.com>
2025-07-07 10:44:15 -0500

    Configure CORS settings for proxy server to allow requests from specific origin

--- index.js
+++ index.js
@@ -6,13 +6,22 @@ const app = express();
 
 const target = "http://52.90.19.163:8080/api";
 
-app.use(cors());
+app.use(cors({
+  origin: "https://flipdot.onrender.com",
+  credentials: true
+}));
+
 app.use(
   "/api",
   createProxyMiddleware({
     target,
     changeOrigin: true,
     pathRewrite: { "^/api": "" },
+    onProxyRes(proxyRes, req, res) {
+      // Set CORS headers manually on the proxied response
+      proxyRes.headers["Access-Control-Allow-Origin"] = "https://flipdot.onrender.com";
+      proxyRes.headers["Access-Control-Allow-Credentials"] = "true";
+    }
   })
 );
 
